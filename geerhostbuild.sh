c58da75817754880f9b8a6cd3645e4e10ca319b4
root <root@localhost.localdomain>
2014-10-21 13:01:45 -0700

    geerhostbuild.sh, geer-data.sql - corrections, updates for A9 Clustering tools

--- geerhostbuild.sh
+++ geerhostbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
-ENDIP=254
-BEGIP=1
+ENDIP=23
+BEGIP=2
 IP=$BEGIP
 SLOT=$IP
 
@@ -15,20 +15,11 @@ od /dev/urandom -w6 -tx1 -An|sed -e 's/ //' -e 's/ /:/g'|head -n 1
 }
 
 
-#(`Datacenter`, `DC_Hostname`, `Clql_Hostname`, `Domain_Name`, `CNAME`, `Public_IP`, `Network`, `DMZ_IP`, `Mgmt_IP`, `Public_MAC`, `DMZ_MAC`, `MGMT_MAC`, `Description`, `ServiceTag`, `Row`, `Rack`, `Slot`)
+#('Datacenter', 'DC_Hostname', 'Clql_Hostname', 'Domain_Name', 'CNAME', 'Public_IP', 'Network', 'DMZ_IP', 'Mgmt_IP', 'Public_MAC', 'DMZ_MAC', 'MGMT_MAC', 'Description', 'ServiceTag', 'Row', 'Rack', 'Slot')
 #('AA', 'b01.co.us.lnx.p01', 'flatirons', 'demo.org', 'flatirons', '192.168.1.1', '192.168.1/24', '192.168.10.1', '192.168.9.1', '00:aa:11:b', 'cc:22:bb:1', '0a:1b:3c:a', 'Colorado SSH access', 'DE7P3DGE', '1', 'A', '0')
 
 make_sql_stub(){
  cat << EOF > geer-data.sql
--- phpMyAdmin SQL Dump
--- version 3.3.10.4
--- http://www.phpmyadmin.net
---
--- Host: mysql.openlpos.org
--- Generation Time: Jun 05, 2012 at 11:04 AM
--- Server version: 5.1.53
--- PHP Version: 5.2.17
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
@@ -38,54 +29,56 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `geer`
+-- Database: 'geer'
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `main`
+-- Table structure for table 'main'
 --
 
-CREATE TABLE IF NOT EXISTS `main` (
-  `Datacenter` varchar(2) DEFAULT NULL,
-  `DC_Hostname` varchar(28) DEFAULT NULL,
-  `Clql_Hostname` varchar(11) DEFAULT NULL,
-  `Domain_Name` varchar(8) DEFAULT NULL,
-  `CNAME` varchar(10) DEFAULT NULL,
-  `Public_IP` varchar(14) DEFAULT NULL,
-  `Network` varchar(16) DEFAULT NULL,
-  `DMZ_IP` varchar(13) DEFAULT NULL,
-  `Mgmt_IP` varchar(12) DEFAULT NULL,
-  `Public_MAC` varchar(10) DEFAULT NULL,
-  `DMZ_MAC` varchar(10) DEFAULT NULL,
-  `MGMT_MAC` varchar(10) NOT NULL DEFAULT '',
-  `Description` varchar(48) DEFAULT NULL,
-  `ServiceTag` varchar(10) DEFAULT NULL,
-  `Row` varchar(1) DEFAULT NULL,
-  `Rack` varchar(1) DEFAULT NULL,
-  `Slot` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`MGMT_MAC`)
+CREATE TABLE IF NOT EXISTS 'main' (
+  'Datacenter' varchar(2) DEFAULT NULL,
+  'DC_Hostname' varchar(28) DEFAULT NULL,
+  'Clql_Hostname' varchar(11) DEFAULT NULL,
+  'Domain_Name' varchar(8) DEFAULT NULL,
+  'CNAME' varchar(10) DEFAULT NULL,
+  'Public_IP' varchar(14) DEFAULT NULL,
+  'Network' varchar(16) DEFAULT NULL,
+  'DMZ_IP' varchar(13) DEFAULT NULL,
+  'Mgmt_IP' varchar(12) DEFAULT NULL,
+  'Public_MAC' varchar(10) DEFAULT NULL,
+  'DMZ_MAC' varchar(10) DEFAULT NULL,
+  'MGMT_MAC' varchar(10) NOT NULL DEFAULT '',
+  'Description' varchar(48) DEFAULT NULL,
+  'ServiceTag' varchar(10) DEFAULT NULL,
+  'Row' varchar(1) DEFAULT NULL,
+  'Rack' varchar(1) DEFAULT NULL,
+  'Slot' varchar(10) DEFAULT NULL,
+  PRIMARY KEY ('MGMT_MAC')
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `main`
+-- Dumping data for table 'main'
 --
-INSERT INTO `main` (`Datacenter`, `DC_Hostname`, `Clql_Hostname`, `Domain_Name`, `CNAME`, `Public_IP`, `Network`, `DMZ_IP`, `Mgmt_IP`, `Public_MAC`, `DMZ_MAC`, `MGMT_MAC`, `Description`, `ServiceTag`, `Row`, `Rack`, `Slot`) VALUES
+INSERT INTO 'main' ('Datacenter', 'DC_Hostname', 'Clql_Hostname', 'Domain_Name', 'CNAME', 'Public_IP', 'Network', 'DMZ_IP', 'Mgmt_IP', 'Public_MAC', 'DMZ_MAC', 'MGMT_MAC', 'Description', 'ServiceTag', 'Row', 'Rack', 'Slot') VALUES
 EOF
 }
 
 make_hostgroup1(){
 
-	echo "('AA', 'b01.co.us.lnx.p0$IP', 'host0$IP', 'demo.com', '', '192.168.1.$IP', '192.168.1/24', '192.168.10.$IP', '192.168.9.$IP','$(randmac)', '$(randmac)', '$(randmac)', 'Geer demo box', '$(randtag)', '1', 'A', '$SLOT')," >> geer-data.sql
+	echo "('570', 'sles113-i386-hdp$IP', 'node0$IP', 'home.a9group.net', '', '10.0.0.$IP', '10.0.0/24', '10.0.2.$IP', '10.0.3.$IP','$(randmac)', '$(randmac)', '$(randmac)', 'HP DL360 G3 - SLES11SP3 - Ambari target cluster', '$(randtag)', '0', 'ZZ', '$IP')," >> geer-data.sql
 
 }
 
 make_hostgroup2(){
 
-	echo "('ZZ', 'd01.tx.us.lnx.d0$IP', 'dev-tx-0$IP', 'dev.demo.com', '', '192.168.3.$IP', '192.168.3/24', '192.168.13.$IP', '192.168.13.$IP','$(randmac)', '$(randmac)', '$(randmac)', 'Geer development box', '$(randtag)', '15', 'H', '$SLOT')," >> geer-data.sql
+	echo "('DEV', 'dev-i386-hdp$IP', 'dev-0$IP', 'dev.a9group.net', '', '192.168.3.$IP', '192.168.3/24', '192.168.13.$IP', '192.168.13.$IP','$(randmac)', '$(randmac)', '$(randmac)', 'HDP development box', '$(randtag)', 'N', '0', '$SLOT')," >> geer-data.sql
 
 }
+
+make_sql_stub
 
 echo ; echo "Building hostgroup 1"
 while [ "$IP" -le "$ENDIP" ]
@@ -96,18 +89,5 @@ do
     SLOT=$IP
 done
 
-ENDIP=254
-BEGIP=1
-IP=$BEGIP
-SLOT=$IP
-
-
-echo ; echo "Building hostgroup 2"
-while [ "$IP" -le "$ENDIP" ]
-do
-    echo -e ".\c"
-    make_hostgroup2
-    IP=$(expr $IP + 1 )
-    SLOT=$IP
-done
-
+echo
+echo
